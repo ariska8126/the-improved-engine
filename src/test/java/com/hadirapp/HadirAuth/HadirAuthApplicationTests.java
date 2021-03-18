126e51319e66c3d0f0c7633f9030ae79a4ec7ba4
ariska8126 <ariska8126@students.amikom.ac.id>
2021-03-18 11:05:29 +0100

    init

---
+++ src/test/java/com/hadirapp/HadirAuth/HadirAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hadirapp.HadirAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HadirAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
