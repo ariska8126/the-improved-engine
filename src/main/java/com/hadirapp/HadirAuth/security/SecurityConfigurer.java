b156274c98cd5d5653e2fb632643874679680d52
ariska8126 <ariska8126@students.amikom.ac.id>
2021-03-23 05:13:55 +0100

    add bcrypt enkripsi & dofilter

--- src/main/java/com/hadirapp/HadirAuth/security/SecurityConfigurer.java
+++ src/main/java/com/hadirapp/HadirAuth/security/SecurityConfigurer.java
@@ -5,6 +5,7 @@
  */
 package com.hadirapp.HadirAuth.security;
 
+import com.hadirapp.HadirAuth.jwtfilter.JwtRequestFilter;
 import com.hadirapp.HadirAuth.jwtutil.JwtUtil;
 import com.hadirapp.HadirAuth.login.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,21 +15,27 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  *
  * @author herli
  */
 @EnableWebSecurity
-public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
-    
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+
     @Autowired
     private MyUserDetailsService myUserDetailsService;
     
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
     @Bean
-    public JwtUtil jwtUtil(){
+    public JwtUtil jwtUtil() {
         return new JwtUtil();
     }
 
@@ -36,16 +43,19 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(myUserDetailsService);
     }
-    
-    
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/auth/resetpassword/**").permitAll()
-                .antMatchers("/authenticate").permitAll();
+                .antMatchers("/authenticate").permitAll()
+                .anyRequest().authenticated().and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
     }
 
     @Override
@@ -53,13 +63,11 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
     protected AuthenticationManager authenticationManager() throws Exception {
         return super.authenticationManager(); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
-    }
-    
-    
 
-    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+//        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
+    }
+
 }
