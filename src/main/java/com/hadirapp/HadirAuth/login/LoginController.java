33b4b1ae849e33fdbc424b0ccd1583196462fc33
Bayu Permana Sejati <47711095+bayuper@users.noreply.github.com>
2021-03-26 08:18:07 +0700

    add check login validation

--- src/main/java/com/hadirapp/HadirAuth/login/LoginController.java
+++ src/main/java/com/hadirapp/HadirAuth/login/LoginController.java
@@ -17,6 +17,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,99 +29,103 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author herli
  */
+@RequestMapping("/api/auth")
 @RestController
 public class LoginController {
-    
+
     @Autowired
     private AuthenticationManager authenticationManager;
-    
+
     @Autowired
     private MyUserDetailsService userDetailsService;
-    
+
     @Autowired
     private JwtUtil jwtTokenUtil;
-    
+
     @Autowired
     JwtRequestFilter jwtRequestFilter;
-    
+
     @Autowired
     private PasswordResetServiceImplement passwordResetServiceImplement;
-    
+
     @Autowired
     UserRepository userRepository;
-    
-    @RequestMapping("/hello")
-    public String hello(){
-        return "hello world";
+
+    @RequestMapping("/checklogin")
+    public String hello() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String nameLogin = auth.getName();
+        JSONObject dataContent = new JSONObject();
+
+        dataContent.put("status", "true");
+        dataContent.put("description", "login success as "+nameLogin);
+        return dataContent.toString();
     }
-    
-    @RequestMapping(value="/authenticate", method = RequestMethod.POST)
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
     public String createAuthenticationToken(
-            @RequestBody AuthenticationRequest authenticationRequest) throws Exception{
-        
+            @RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+
         JSONArray jsonArray = new JSONArray();
         JSONObject jsonObject = new JSONObject();
-        
+
         JSONObject dataContent = new JSONObject();
-        
+
         System.out.println("running login controller");
-        try{
+        try {
             authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), 
-                    authenticationRequest.getPassword())
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(),
+                            authenticationRequest.getPassword())
             );
-        }catch(BadCredentialsException e){
+        } catch (BadCredentialsException e) {
             //throw new Exception("incorect username or password"+e);
-            dataContent.put("status","false");
-            dataContent.put("description","incorrect email or password");
-            
+            dataContent.put("status", "false");
+            dataContent.put("description", "incorrect email or password");
+
             return dataContent.toString();
         }
-        
-        
-        
+
         final UserDetails userDetails = userDetailsService
                 .loadUserByUsername(authenticationRequest.getUsername());
-        
-        System.out.println("username to insert token: "+userDetails.getUsername());
-        
+
+        System.out.println("username to insert token: " + userDetails.getUsername());
+
         String uname = userDetails.getUsername();
-        
+
         Users users = new Users();
         users = passwordResetServiceImplement.findByEmail(uname);
         String userToken = users.getUserToken();
-        
-        
+
         String id = users.getUserId();
         String email = users.getUserEmail();
         int roleId = users.getRoleId().getRoleId();
         String roleName = users.getRoleId().getRoleName();
         String name = users.getUserFullname();
-        
+
 //        System.out.println("id: "+id);
 //        System.out.println("email: "+email);
 //        System.out.println("role id: "+roleId);
 //        System.out.println("role name: "+roleName);
         jsonObject.put("status", "true");
-        
-        dataContent.put("userFullname",name);
+
+        dataContent.put("userFullname", name);
         dataContent.put("userEmail", email);
         dataContent.put("userId", id);
         dataContent.put("roleId", roleId);
-        dataContent.put("roleName",roleName);
+        dataContent.put("roleName", roleName);
         dataContent.put("divisionId", users.getDivisionId().getDivisionId().toString());
         dataContent.put("divisionName", users.getDivisionId().getDivisionName());
-        
+
         final String jwt = jwtTokenUtil.generateToken(userDetails, dataContent);
         dataContent.put("userPhoto", users.getUserPhoto());
         //dataContent.put("status", "true");
         jsonArray.add(dataContent);
         jsonObject.put("userToken", jwt);
         jsonObject.put("data", jsonArray);
-        
+
         users.setUserToken(jwt);
         userRepository.save(users);
         return jsonObject.toJSONString();
     }
-    
+
 }
