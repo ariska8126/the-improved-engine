622fd2f6168a0a3eccb7cc081d9ed4cb00406c1e
Bayu Permana Sejati <47711095+bayuper@users.noreply.github.com>
2021-03-26 08:00:48 +0700

    add additional data source

--- src/main/java/com/hadirapp/HadirAuth/HadirAuthApplication.java
+++ src/main/java/com/hadirapp/HadirAuth/HadirAuthApplication.java
@@ -2,12 +2,21 @@ package com.hadirapp.HadirAuth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HadirAuthApplication {
+public class HadirAuthApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HadirAuthApplication.class, args);
 	}
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HadirAuthApplication.class);
+    }
+        
+        
 
 }
